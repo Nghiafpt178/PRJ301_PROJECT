918d9126598d4a4a62637a065d98692f90af2c98
Nguyen Nghia <75856114+Nghiafpt178@users.noreply.github.com>
2021-10-27 19:27:06 +0700

    Add files via upload

---
+++ PROJECT_PRJ301_BookingTour/src/java/controller/admin/DeleteTourController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.admin;
+
+import dal.TourDBContext;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DeleteTourController extends HttpServlet {
+
+   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String tcode = request.getParameter("tourcode");
+        TourDBContext tourDB = new TourDBContext();
+        tourDB.deleteTours(tcode);
+        response.sendRedirect("ToursManager");
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         processRequest(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         processRequest(request, response);
+    }
+
+
+}
