d45719dc1c889f39ff1b4c57d8ab6b86ba2e74ce
Nguyen Nghia <75856114+Nghiafpt178@users.noreply.github.com>
2021-10-27 19:29:13 +0700

    Update ToursManager.java

--- PROJECT_PRJ301_BookingTour/src/java/controller/admin/ToursManager.java
+++ PROJECT_PRJ301_BookingTour/src/java/controller/admin/ToursManager.java
@@ -21,21 +21,50 @@ import model.Tour;
  */
 public class ToursManager extends HttpServlet {
 
-   
-    @Override
+  @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         TourDBContext tourDB = new TourDBContext();
         ArrayList<Tour> tours = tourDB.getTours();
+        ArrayList<ToursType> toursType = tourDB.getToursType();
+        request.setAttribute("toursType", toursType);
         request.setAttribute("tours", tours);
         
         request.getRequestDispatcher("/view/admin/ToursManager.jsp").forward(request, response);
     }
-
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String tcode = request.getParameter("tcode");
+        String tname = request.getParameter("tname");
+        String timg = request.getParameter("timg");
+        int tprice = Integer.parseInt(request.getParameter("tprice"));
+        Date tstartDate = Date.valueOf(request.getParameter("tstartDate"));
+        Date tendDate = Date.valueOf(request.getParameter("tendDate"));
+        String tduration = request.getParameter("tduration");
+        String tvehicle = request.getParameter("tvehicle");
+        String tdescip = request.getParameter("tdescip");
+        String tschedule = request.getParameter("tschedule");
+        int tourType = Integer.parseInt(request.getParameter("ttype"));
+        int tourNumber = Integer.parseInt(request.getParameter("tnumber"));
+        Tour t = new Tour();
+        t.setTourCode(tcode);
+        t.setTourName(tname);
+        t.setImg(timg);
+        t.setTourPrice(tprice);
+        t.setStartDate(tstartDate);
+        t.setEndDate(tendDate);
+        t.setDateTime(tduration);
+        t.setVehicle(tvehicle);
+        t.setDescription(tdescip);
+        t.setSchedule(tschedule);
+        t.setTourType(tourType);
+        t.setNumberGuests(tourNumber);
+        
+        TourDBContext tourDB = new TourDBContext();
+        tourDB.saveTours(t);
+        response.sendRedirect("ToursManager");
         
     }
 
