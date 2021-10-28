37713d5efb0f1d5707633f009ef7503c1b0e244b
Nguyen Nghia <75856114+Nghiafpt178@users.noreply.github.com>
2021-10-29 00:22:07 +0700

    Add files via upload

---
+++ PROJECT_PRJ301_BookingTour/src/java/model/BookTours_Status.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class BookTours_Status {
+    private int id;
+    private String status_name;
+
+    public BookTours_Status() {
+    }
+
+    public BookTours_Status(int id, String status_name) {
+        this.id = id;
+        this.status_name = status_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus_name() {
+        return status_name;
+    }
+
+    public void setStatus_name(String status_name) {
+        this.status_name = status_name;
+    }
+    
+    
+    
+}
