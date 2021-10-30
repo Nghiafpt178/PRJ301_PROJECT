5eac0240a9385c5ba9c690fd807c6212b12fd90a
Nguyen Nghia <75856114+Nghiafpt178@users.noreply.github.com>
2021-10-30 23:55:10 +0700

    Add files via upload

---
+++ PROJECT_PRJ301_BookingTour/src/java/model/Group.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Group {
+    private int gid;
+    private String gname;
+
+    public Group() {
+    }
+
+    public Group(int gid, String gname) {
+        this.gid = gid;
+        this.gname = gname;
+    }
+
+    public int getGid() {
+        return gid;
+    }
+
+    public void setGid(int gid) {
+        this.gid = gid;
+    }
+
+    public String getGname() {
+        return gname;
+    }
+
+    public void setGname(String gname) {
+        this.gname = gname;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" + "gid=" + gid + ", gname=" + gname + '}';
+    }
+    
+    
+    
+}
