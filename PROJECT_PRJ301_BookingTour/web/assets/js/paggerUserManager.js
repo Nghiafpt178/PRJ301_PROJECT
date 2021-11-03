996acf0cfd75f7114b7d0cbfda275cceb929ac53
Nguyen Nghia <75856114+Nghiafpt178@users.noreply.github.com>
2021-11-04 00:43:29 +0700

    Add files via upload

---
+++ PROJECT_PRJ301_BookingTour/web/assets/js/paggerUserManager.js
@@ -0,0 +1,26 @@
+
+
+function createPaggerUser(div, pageindex, gap, totalpage)
+{
+    var container = document.getElementById(div);
+    //container.innerHTML = pageindex + ' ' + gap + ' '+ totalpage;
+    if (pageindex - gap > 1)
+        container.innerHTML += '<a href="userManager?page=1">First</a>';
+
+
+
+    for (var i = pageindex - gap; i < pageindex; i++)
+    {
+        if (i > 0)
+            container.innerHTML += '<a style="background: whitesmoke; padding: 10px; margin-bottom: 30px;" href="userManager?page=' + i + '">' + i + '</a>';
+    }
+    container.innerHTML += '<span>' + pageindex + '</span>';
+
+    for (var i = pageindex + 1; i <= pageindex + gap; i++)
+    {
+        if (i <= totalpage)
+            container.innerHTML += '<a style="background: whitesmoke; padding: 10px; margin-bottom: 30px;" href="userManager?page=' + i + '">' + i + '</a>';
+    }
+    if (pageindex + gap < totalpage)
+        container.innerHTML += '<a style="background: whitesmoke; padding: 10px; margin-bottom: 30px;" href="userManager?page=' + totalpage + '">Last</a>';
+}
