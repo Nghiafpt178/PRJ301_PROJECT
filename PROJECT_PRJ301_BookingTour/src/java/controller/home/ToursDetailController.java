b2f99e85d374a4dac31470ad39abfd380fb0979e
Nguyen Nghia <75856114+Nghiafpt178@users.noreply.github.com>
2021-11-05 17:58:24 +0700

    Update ToursDetailController.java

--- PROJECT_PRJ301_BookingTour/src/java/controller/home/ToursDetailController.java
+++ PROJECT_PRJ301_BookingTour/src/java/controller/home/ToursDetailController.java
@@ -19,25 +19,51 @@ import model.Tour;
  * @author ADMIN
  */
 public class ToursDetailController extends HttpServlet {
-    
+   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String username = (String) request.getSession().getAttribute("username");
+        String tourCode = request.getParameter("tcode");
+        TourDBContext tourDB = new TourDBContext();
+        Tour tourByCode = tourDB.getToursByCode(tourCode);
+        request.setAttribute("tourByCode", tourByCode);
+         CommentDBContext commentDB = new CommentDBContext();
+        int total = commentDB.countComment(tourCode);
+        ArrayList<Comment> comments = commentDB.getComments(tourCode);
+        request.setAttribute("comments", comments);
+        request.setAttribute("user", username);
+        request.setAttribute("total", total);
+        Comment c = new Comment();
+        String content = request.getParameter("content");
+
+        c.setTourCode(tourCode);
+        c.setUsername(username);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        c.setDatetime(dtf.format(now));
+        c.setContent(content);
+        CommentDBContext cDB = new CommentDBContext();
+        cDB.insertComment(c);
+
+        request.getRequestDispatcher("/view/tour/ToursDetail.jsp").forward(request, response);
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-         String tourCode = request.getParameter("tcode");
-        TourDBContext  tourDB= new TourDBContext();
-        Tour tourByCode = tourDB.getToursByCode(tourCode);
-        request.setAttribute("tourByCode", tourByCode);  
         
-        
-        request.getRequestDispatcher("/view/tour/ToursDetail.jsp").forward(request, response);
-     
-        
+
+       
+        processRequest(request, response);
+
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
+        processRequest(request, response);
     }
     
 }
