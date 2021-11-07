be7f672220390903b16de8996a4024fe40ce1cf3
Nguyen Nghia <75856114+Nghiafpt178@users.noreply.github.com>
2021-11-08 00:02:01 +0700

    Update AdminController.java

--- PROJECT_PRJ301_BookingTour/src/java/controller/admin/AdminController.java
+++ PROJECT_PRJ301_BookingTour/src/java/controller/admin/AdminController.java
@@ -23,7 +23,20 @@ public class AdminController extends BaseRequiredAuthenController {
     @Override
     protected void processGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("../view/admin/Admin.jsp").forward(request, response);
+         DecentraliseDBContext decenDB = new DecentraliseDBContext();
+        int row_Acc = decenDB.getRowCount();
+        request.setAttribute("totalAcc", row_Acc);
+        TourDBContext tourDB = new TourDBContext();
+        int total_Tour = tourDB.getRowCount();
+        request.setAttribute("totalTour", total_Tour);
+        NewsDBContext newDB = new NewsDBContext();
+        int totalNews = newDB.getRowCount();
+        request.setAttribute("totalNews", totalNews);
+        int totalComment = decenDB.getRowCountComment();
+        request.setAttribute("totalComment", totalComment);
+        int totalTourBooked = tourDB.getRowCountBooked();
+        request.setAttribute("totalTourBooked", totalTourBooked);
+        request.getRequestDispatcher("../view/admin/DashBoard.jsp").forward(request, response);
     }
 
   
